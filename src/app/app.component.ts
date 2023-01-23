94144684ed7baeacba34f4dd7d7ceb2666f4e864
diego.leguizamon <diegog_legui@hotmail.com>
2023-01-23 12:24:36 -0300

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'This is my first app in Angular';
+  saludos = 'greetings from Angular Service';
 }
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-personas',
+    /* templateUrl: './personas.component.html', */
+    /* inline template*/template: `<h1>Listado de Personas</h1>
+    <app-persona></app-persona>
+    <app-persona></app-persona>`
+})
+export class PersonasComponent{
+
+}
