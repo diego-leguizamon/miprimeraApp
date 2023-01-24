d48f05ea4ecddbf413d39f01c9f701b0ae5f8342
diego.leguizamon <diegog_legui@hotmail.com>
2023-01-24 16:25:02 -0300

    avanze 1

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-personas',
-    /* templateUrl: './personas.component.html', */
-    /* inline template*/template: `<h1>Listado de Personas</h1>
+    templateUrl: './personas.component.html', 
+    /* inline templatetemplate: `<h1>Listado de Personas 2023</h1>
     <app-persona></app-persona>
-    <app-persona></app-persona>`
+    <app-persona></app-persona>`*/
+    // styleUrls: ['./personas.component.css'] 
+    styles: [`
+        h1{color: blue;}
+    `]
 })
 export class PersonasComponent{
 
