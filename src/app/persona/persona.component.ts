ec06a9e65a6d6591ce8f34b0881399779530394b
diego.leguizamon <diegog_legui@hotmail.com>
2023-01-24 17:58:29 -0300

    binding y otros ejemplos

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class PersonaComponent{
   
-  constructor(){}
+  //constructor(){}
+  //atributos publicos para utilizar interpolacion
+  nombre: string = 'Juan';
+  apellido: string = 'Perez';
+  edad: number = 28;
+  //si tengo un atributo privado, utilizariamos un get, ejemplo telefono
+  private telefono: number = 3794006989;
+
+  getTelefono(): number{
+    return this.telefono;
+  }
+
 }
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,14 +3,26 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-personas',
     templateUrl: './personas.component.html', 
-    /* inline templatetemplate: `<h1>Listado de Personas 2023</h1>
+    /* inline template template: `<h1>Listado de Personas 2023</h1>
     <app-persona></app-persona>
     <app-persona></app-persona>`*/
     // styleUrls: ['./personas.component.css'] 
+    // inline styles 
     styles: [`
         h1{color: blue;}
     `]
 })
 export class PersonasComponent{
 
+    deshabilitar = false;
+    mensaje = 'No se ha agregado ninguna persona';
+    titulo = '';
+    twoway ='probamos';
+    
+    agregarPersona(){
+        this.mensaje = 'Pesona agregada'
+    }
+    modificarTitulo(event: Event){
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }
 }
